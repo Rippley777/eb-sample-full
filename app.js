3172d00b5054d0b3361981eaa549aa7f89ec872d
Rippley777 <ally.rippley@gmail.com>
2022-10-05 02:22:15 -0500

    add react routes

--- app.js
+++ app.js
@@ -40,58 +40,62 @@ if (cluster.isMaster) {
 
     // app.set('view engine', 'ejs');
     // app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
     app.use(bodyParser.urlencoded({extended:false}));
     app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-
-    app.get('/', function(req, res) {
-        res.render('index', {
-            static_path: 'static',
-            theme: process.env.THEME || 'flatly',
-            flask_debug: process.env.FLASK_DEBUG || 'false'
-        });
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
     });
+    
+    // app.get('/', function(req, res) {
+    //     res.render('index', {
+    //         static_path: 'static',
+    //         theme: process.env.THEME || 'flatly',
+    //         flask_debug: process.env.FLASK_DEBUG || 'false'
+    //     });
+    // });
 
-    app.post('/signup', function(req, res) {
-        var item = {
-            'email': {'S': req.body.email},
-            'name': {'S': req.body.name},
-            'preview': {'S': req.body.previewAccess},
-            'theme': {'S': req.body.theme}
-        };
+    // app.post('/signup', function(req, res) {
+    //     var item = {
+    //         'email': {'S': req.body.email},
+    //         'name': {'S': req.body.name},
+    //         'preview': {'S': req.body.previewAccess},
+    //         'theme': {'S': req.body.theme}
+    //     };
 
-        ddb.putItem({
-            'TableName': ddbTable,
-            'Item': item,
-            'Expected': { email: { Exists: false } }        
-        }, function(err, data) {
-            if (err) {
-                var returnStatus = 500;
+    //     ddb.putItem({
+    //         'TableName': ddbTable,
+    //         'Item': item,
+    //         'Expected': { email: { Exists: false } }        
+    //     }, function(err, data) {
+    //         if (err) {
+    //             var returnStatus = 500;
 
-                if (err.code === 'ConditionalCheckFailedException') {
-                    returnStatus = 409;
-                }
+    //             if (err.code === 'ConditionalCheckFailedException') {
+    //                 returnStatus = 409;
+    //             }
 
-                res.status(returnStatus).end();
-                console.log('DDB Error: ' + err);
-            } else {
-                sns.publish({
-                    'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
-                                        + "\r\nPreviewAccess: " + req.body.previewAccess 
-                                        + "\r\nTheme: " + req.body.theme,
-                    'Subject': 'New user sign up!!!',
-                    'TopicArn': snsTopic
-                }, function(err, data) {
-                    if (err) {
-                        res.status(500).end();
-                        console.log('SNS Error: ' + err);
-                    } else {
-                        res.status(201).end();
-                    }
-                });            
-            }
-        });
-    });
+    //             res.status(returnStatus).end();
+    //             console.log('DDB Error: ' + err);
+    //         } else {
+    //             sns.publish({
+    //                 'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
+    //                                     + "\r\nPreviewAccess: " + req.body.previewAccess 
+    //                                     + "\r\nTheme: " + req.body.theme,
+    //                 'Subject': 'New user sign up!!!',
+    //                 'TopicArn': snsTopic
+    //             }, function(err, data) {
+    //                 if (err) {
+    //                     res.status(500).end();
+    //                     console.log('SNS Error: ' + err);
+    //                 } else {
+    //                     res.status(201).end();
+    //                 }
+    //             });            
+    //         }
+    //     });
+    // });
 
     var port = process.env.PORT || 3000;
 
