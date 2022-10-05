5519bf7c3dc573a62731f74639aa5e2e121629a5
Rippley777 <ally.rippley@gmail.com>
2022-10-05 02:24:29 -0500

    add react routes

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ if (cluster.isMaster) {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
     
     // app.get('/', function(req, res) {
